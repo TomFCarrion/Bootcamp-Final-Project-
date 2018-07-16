c5ee45c569dcbe12d61ff6f926dfd52175df93f3
Tomas F. Carrion <tomfcarrion@gmail.com>
2018-07-16 02:33:57 -0300

    Create Header & Form
    Header done,
    Form working properly, except radio input.

--- githubjobs/src/App.js
+++ githubjobs/src/App.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
+import Header from './components/header/header';
+import Form from './components/form/form';
+
 import './App.css';
 
+
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+          location: '',
+          jobDescription: '',
+          fulltime: ''
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit(data){
+    this.setState({
+      location: data.location,
+      jobDescription: data.jobDescription
+    })
+  }
+
   render() {
     return (
       <div className="App">
+      <Header />
+      <Form  handleSubmit={this.handleSubmit}/>
 
+        <label>
+          result:<br></br>
+          location:<br></br>
+            {this.state.location}
+            <br></br>
+          job:
+            {this.state.jobDescription}
+        </label>
       </div>
     );
   }
---
+++ githubjobs/src/components/form/form.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import './form.css';
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: '',
+      jobDescription: '',
+      fulltime:''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    this.props.handleSubmit(this.state);
+    this.setState({
+          location: '',
+          jobDescription: '',
+          fulltime: ''
+        }); // empty the field.
+        event.preventDefault();
+
+  }
+
+  render() {
+    return (
+
+      <form className="input" onSubmit={this.handleSubmit}>
+        <h1>Find your job!</h1>
+        <label className="item">
+          <input type="text"  placeholder="Location" name="location" value={this.state.location} onChange={this.handleChange} />
+        </label>
+        <label className="item">
+          <input type="text"  placeholder="Job description" name="jobDescription" value={this.state.jobDescription} onChange={this.handleChange} />
+        </label>
+        <label className="item">
+          <input type="radio" name="fulltime" value={this.state.fulltime} onChange={this.handleChange}   />
+          Full Time?
+        </label>
+
+        <input className="button" type="submit" value="Search" />
+      </form>
+    );
+  }
+}
+
+
+export default Form;
