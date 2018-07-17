a30aab3096b68c3797b7e987491f34d2a67daf1b
Juan M. Rivas <38479624+juanm495@users.noreply.github.com>
2018-07-17 00:36:45 -0300

    non working fetch (#1)
    * non working fetch
    
    added arrow func + fetch() in dev.
    
    * added working fetch
    
    * replaced bind() with arrow functions
    
    * deleted misplaced function
    
    * arrow functions

--- githubjobs/src/App.js
+++ githubjobs/src/App.js
@@ -15,11 +15,11 @@ class App extends Component {
           fulltime: ''
     };
 
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.fetchData = this.fetchData.bind(this)
+    this.handleSubmit = this.handleSubmit
+    this.fetchData = this.fetchData
   }
 
-  handleSubmit(data){
+  handleSubmit = (data) =>{
     this.setState({
       location: data.location,
       jobDescription: data.jobDescription
@@ -31,8 +31,8 @@ class App extends Component {
   //   this.fetchData();
   // }
 
-  fetchData(data){
-    fetch('/positions.json?description='+ data.jobDescription + '&location=' + data.location)
+  fetchData = (data) => {
+    fetch('jobs.github.com/positions.json?description='+ data.jobDescription + '&location=' + data.location)
     .then(response => response.json())
     .then(response => {
       console.log(response);
@@ -44,7 +44,7 @@ class App extends Component {
       <div className="App">
       <Header />
       <Form  handleSubmit={this.handleSubmit}/>
-
+      
         <label>
           result:<br></br>
           location:<br></br>
--- githubjobs/src/components/form/form.js
+++ githubjobs/src/components/form/form.js
@@ -10,17 +10,18 @@ class Form extends Component {
       fulltime:''
     };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange;
+    this.handleSubmit = this.handleSubmit;
   }
 
-  handleChange(event) {
+  //no need to use bind() if you use arrow functions as they do it automatically
+  handleChange = (event) => {
     this.setState({
       [event.target.name]: event.target.value
     });
   }
 
-  handleSubmit(event) {
+  handleSubmit = (event) => {
     this.props.handleSubmit(this.state);
     this.setState({
           location: '',
@@ -30,7 +31,6 @@ class Form extends Component {
         event.preventDefault();
 
   }
-
   render() {
     return (
 
